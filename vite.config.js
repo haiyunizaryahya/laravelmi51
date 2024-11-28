b928f7a47c8a69fb6e6da036801499b75ce33c93
haiyunizaryahya <162580834+haiyunizaryahya@users.noreply.github.com>
2024-11-28 18:28:23 +0700

    npmrun

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,9 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-    ],
+    ], build: {
+        manifest: true,
+        outDir: 'public', // or your specified output directory
+      }
+
 });
