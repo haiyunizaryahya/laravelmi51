88214ecc26dee1feeb033fe0d18cf1ed500dd348
haiyunizaryahya <162580834+haiyunizaryahya@users.noreply.github.com>
2024-12-03 17:30:01 +0700

    login
    login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -28,7 +28,7 @@
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
-            <a class="navbar-brand ps-3" href="index.html">Aplikasi Perawatan Sarana</a>
+            <a class="navbar-brand ps-3" href="index.html">Aplikasi  Laporan Perawatan</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
                 <i class="fas fa-bars"></i>
             </button>
